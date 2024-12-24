a6c43bb3c30ccf17df9b8d096558a0590a780693
Rocky7842 <eric.rocky7842@gmail.com>
2024-12-24 20:57:34 +0800

    xmsirius: Adapt props and fingerprint overrides for V
    Change-Id: I41a51b9d1ce64b7f71b6ef40079e202f856bfb70

--- lineage_xmsirius.mk
+++ lineage_xmsirius.mk
@@ -24,6 +24,5 @@ PRODUCT_MODEL := MI 8 SE
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sirius-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/sirius/sirius:10/QKQ1.190828.002/V12.5.1.0.QEBCNXM:user/release-keys
+    BuildDesc="sirius-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys" \
+    BuildFingerprint=Xiaomi/sirius/sirius:10/QKQ1.190828.002/V12.5.1.0.QEBCNXM:user/release-keys
