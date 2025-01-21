eab578a368980a15e59d30ef3dfc89be1ca40253
Rocky7842 <eric.rocky7842@gmail.com>
2025-01-21 22:08:35 +0800

    xmsirius: Switch to py extract utils
    Change-Id: Ia044f615a093ac4153551a42ea3be8cf903d6574

---
+++ extract-files.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom/sdm845',
+    'hardware/xiaomi',
+    'vendor/xiaomi/sdm710-common',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib/libswregistrationalgo.so', 'vendor/lib/hw/camera.qcom.so', 'vendor/lib64/libsensorcal.so', 'vendor/lib64/sensors.ssc.so'): blob_fixup()
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-v29.so'),
+    'vendor/lib/camera/components/com.qti.node.watermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'xmsirius',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sdm710-common', module.vendor)
+    utils.run()
