3d0a9de5ddcb432e1856b250deb1c24f07ba497d
Rocky7842 <eric.rocky7842@gmail.com>
2026-02-22 15:44:32 +0800

    xmsirius: Update namespace import to qcom-caf
    Change-Id: I3eda9d3ee21192b39ff42d577c0101532fe0bf0a

--- extract-files.py
+++ extract-files.py
@@ -22,8 +22,9 @@ from extract_utils.main import (
 
 namespace_imports = [
     'hardware/qcom-caf/common/libqti-perfd-client',
-    'hardware/qcom/sdm845/display',
+    'hardware/qcom-caf/sdm845',
     'hardware/xiaomi',
+    'vendor/qcom/opensource/display',
     'vendor/xiaomi/sdm710-common',
 ]
 
